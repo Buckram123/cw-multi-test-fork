333228c957b65f7ddbf4b2e5f921f6ce8a14b14b
Buckram <buckram123@gmail.com>
2024-07-04 17:10:31 +0300

    remote channel only needs ChainInfo

--- src/wasm_emulation/channel.rs
+++ src/wasm_emulation/channel.rs
@@ -4,11 +4,7 @@ use tokio::runtime::{Handle, Runtime};
 use tonic::transport::Channel;
 
 /// Simple helper to get the GRPC transport channel
-fn get_channel(
-    chain: impl Into<ChainInfoOwned>,
-    rt: &Runtime,
-) -> anyhow::Result<tonic::transport::Channel> {
-    let chain = chain.into();
+fn get_channel(chain: &ChainInfoOwned, rt: &Runtime) -> anyhow::Result<tonic::transport::Channel> {
     let channel = rt.block_on(GrpcChannel::connect(&chain.grpc_urls, &chain.chain_id))?;
     Ok(channel)
 }
@@ -18,18 +14,18 @@ pub struct RemoteChannel {
     pub rt: Handle,
     pub channel: Channel,
     pub pub_address_prefix: String,
+    // For caching
+    pub chain_id: String,
 }
 
 impl RemoteChannel {
-    pub fn new(
-        rt: &Runtime,
-        chain: impl Into<ChainInfoOwned>,
-        pub_address_prefix: impl Into<String>,
-    ) -> AnyResult<Self> {
+    pub fn new(rt: &Runtime, chain: impl Into<ChainInfoOwned>) -> AnyResult<Self> {
+        let chain: ChainInfoOwned = chain.into();
         Ok(Self {
             rt: rt.handle().clone(),
-            channel: get_channel(chain, rt)?,
-            pub_address_prefix: pub_address_prefix.into(),
+            channel: get_channel(&chain, rt)?,
+            pub_address_prefix: chain.network_info.pub_address_prefix,
+            chain_id: chain.chain_id,
         })
     }
 }
